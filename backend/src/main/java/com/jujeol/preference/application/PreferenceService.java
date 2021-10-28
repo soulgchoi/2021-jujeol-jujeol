6e397006711aace81abcea000f659cd6824c33fe
bomin <bomin_93@naver.com>
2021-10-28 10:46:50 +0900

    feat: preference 추가, 삭제, 업데이트 시 dataMatrix 수정 코드 실행

--- backend/src/main/java/com/jujeol/preference/application/PreferenceService.java
+++ backend/src/main/java/com/jujeol/preference/application/PreferenceService.java
@@ -3,6 +3,8 @@ package com.jujeol.preference.application;
 import com.jujeol.drink.drink.domain.Drink;
 import com.jujeol.drink.drink.domain.repository.DrinkRepository;
 import com.jujeol.drink.drink.exception.NotFoundDrinkException;
+import com.jujeol.drink.recommend.infrastructure.slope.DataMatrix;
+import com.jujeol.drink.recommend.infrastructure.slope.DataModel;
 import com.jujeol.member.member.application.dto.PreferenceDto;
 import com.jujeol.preference.domain.Preference;
 import com.jujeol.preference.domain.PreferenceRepository;
@@ -18,6 +20,7 @@ public class PreferenceService {
 
     private final PreferenceRepository preferenceRepository;
     private final DrinkRepository drinkRepository;
+    private final DataMatrix dataMatrix;
 
     public Page<Preference> showPreferenceByMemberId(Long memberId, Pageable pageable) {
         return preferenceRepository.findByMemberIdOrderByCreatedAtDesc(memberId, pageable);
@@ -44,13 +47,20 @@ public class PreferenceService {
         Drink drink = drinkRepository.findById(drinkId)
                 .orElseThrow(NotFoundDrinkException::new);
 
+        final DataModel dataModel = new DataModel(memberId, drinkId, preferenceDto.getPreferenceRate());
+
         preferenceRepository
                 .findByMemberIdAndDrinkId(memberId, drink.getId())
-                .ifPresentOrElse(exist -> exist.updateRate(preferenceDto.getPreferenceRate()),
+                .ifPresentOrElse(exist -> {
+                    exist.updateRate(preferenceDto.getPreferenceRate());
+                    dataMatrix.updateData(
+                            dataModel);
+                        },
                         () -> {
                             Preference newPreference = Preference
                                     .create(memberId, drink, preferenceDto.getPreferenceRate());
                             preferenceRepository.save(newPreference);
+                            dataMatrix.addData(dataModel);
                         }
                 );
 
@@ -64,6 +74,7 @@ public class PreferenceService {
 
         preferenceRepository.deleteByMemberIdAndDrinkId(memberId, drinkId);
 
+        dataMatrix.removeData(memberId, drinkId);
         updatePreferenceAverage(drinkId, drink);
     }
 
@@ -74,5 +85,6 @@ public class PreferenceService {
 
     public void deletePreferenceByDrinkId(Long id) {
         preferenceRepository.deleteByDrinkId(id);
+        dataMatrix.resetData();
     }
 }
